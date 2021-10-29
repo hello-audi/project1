fc95c5b26723decfe8c11f923fee1170b43374f3
ychen304 <ychen304@hawk.iit.edu>
2021-10-28 19:58:42 -0500

    fix cascade of all files

--- js/script.js
+++ js/script.js
@@ -1,30 +1,23 @@
 function myFunction() {
   var checkBox = document.getElementById("dnewsletter");
   var text = document.getElementById("text");
-  if (!checkBox.checked == true)
-  {
+  if (!checkBox.checked == true) {
     text.style.display = "none";
-  } 
-  else 
-  {
-     text.style.display = "block";
+  } else {
+    text.style.display = "block";
   }
 }
 
-function smbt()
-{
-	document.getElementById("submit").addEventListener("click", function() 
-		{
-			var output = "Welcome to the ITM family, " + document.forms["form"]["fname"].value + " " + document.forms["form"]["lname"].value;
-			alert(output);
-  		});
+function smbt() {
+  document.getElementById("submit").addEventListener("click", function () {
+    var output = "Welcome to the ITM family, " + document.forms["form"]["fname"].value + " " + document.forms["form"]["lname"].value;
+    alert(output);
+  });
 
-	document.getElementById("submit").addEventListener("click", function() 
-		{
-			var output = "You deserve the best from us!"
-			alert(output);
-  		});
+  document.getElementById("submit").addEventListener("click", function () {
+    var output = "You deserve the best from us!"
+    alert(output);
+  });
 }
 
 window.addEventListener('load', smbt);
-
